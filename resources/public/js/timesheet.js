fc7c74f4f7e786abeb2505052876415da2893ba7
Justin Good <jgoodhcg@gmail.com>
2016-02-25 23:07:32 -0500

    optimized range parsing

--- resources/public/js/timesheet.js
+++ resources/public/js/timesheet.js
@@ -1,19 +1,57 @@
 var timesheet = (function(){
   // instance variables
-  var width, height;
+  // TODO will use more than once
+  var width, height, $container, chart;
+  // TODO possibly will use more than once
+  var $window, heightpad = 170;
   // internal functions
 
   // external functions
   return{
-    display: function(cont){
-      container = document.getElementById(cont);
-      width = container.offsetWidth;
-      height = ;
+    create: function(params){
+      // check params
+      var req_keys = ['csv', 'cont'],
+      missing_keys = _.difference(req_keys, _.keys(params));
+      if(missing_keys.length > 0){
+        throw("ERROR parameter(s) missing: "+missing_keys.join(', '));
+      }
+      // cache dom
+      $container = $("#"+params.cont);
+      $window = $(window);
 
-      console.log(width);
-      console.log(height);
+      width = $container.width();
+      height = $window.innerHeight() - heightpad;
+
+      chart = d3.select("#"+params.cont)
+      .append("div")
+      .attr("id", params.cont+'-svg-container')
+      .attr("class", "svg-container")
+      .attr("width", width)
+      .attr("height", height);
+
+      chart
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("id", params.cont+"-svg");
+
+      // load csv data
+      d3.csv(params.csv, function(d) {return d;},
+      function(error, tasks){
+        // bounding dates
+        var range_accessor = function(task, i){
+          return moment(task.Date, "MM/DD/YY").unix();
+        }
+        var range = moment.range(
+          moment.unix(d3.min(tasks, range_accessor)),
+          moment.unix(d3.max(tasks, range_accessor)));
+
+      // create all days array
+
+      // iterate tasks to fill all days
+      });
     }
   }
 })();
 
-timesheet.display("container");
+timesheet.create({cont:"container", csv:"/assets/winter2013.csv"});

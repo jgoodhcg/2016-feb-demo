15f5c84b6b34ad6d89581ca0a7fe4c6b80386e5a
Justin Good <jgoodhcg@gmail.com>
2016-02-26 22:08:32 -0500

    parsing progress

--- resources/public/js/timesheet.js
+++ resources/public/js/timesheet.js
@@ -1,11 +1,39 @@
 var timesheet = (function(){
   // instance variables
+
   // TODO will use more than once
-  var width, height, $container, chart;
+  var width, height, $container, chart, range,
+  all_days = [], days = [],
+  project_colors = { },
+  break_reg = /(\d{1,2}:\d{2}[ap]m\s*–\s*\d{1,2}:\d{2}[ap]m)(\D*(?:\d(?!\d?:\d{2}[ap]m\s)\D*)*)/ig,
+  time_stamp_reg = /(\d{1,2}:\d{2}[ap]m\s*–\s*\d{1,2}:\d{2}[ap]m)/;
+
   // TODO possibly will use more than once
   var $window, heightpad = 170;
-  // internal functions
 
+  // internal functions
+  function meridiem(time){
+    var m = time.charAt(time.length-2),
+    r = 'error';
+    switch (m) {
+      case 'P':
+      r = 'A'; // post meridiem in moment.js
+      break;
+
+      case 'A':
+      r = 'a'; // ante meridiem in moment.js
+      break;
+
+      case 'p':
+      r = 'A'; // post meridiem in moment.js
+      break;
+
+      case 'a':
+      r = 'a'; // ante meridiem in moment.js
+      break;
+    }
+    return r;
+  }
   // external functions
   return{
     create: function(params){
@@ -38,20 +66,73 @@ var timesheet = (function(){
       // load csv data
       d3.csv(params.csv, function(d) {return d;},
       function(error, tasks){
-        // bounding dates
+        // create date range
         var range_accessor = function(task, i){
           return moment(task.Date, "MM/DD/YY").unix();
         }
-        var range = moment.range(
+        range = moment.range(
           moment.unix(d3.min(tasks, range_accessor)),
           moment.unix(d3.max(tasks, range_accessor)));
 
-      // create all days array
+          // fill all_days
+          range.by('days', function(m){
+            all_days.push({date: m, intervals: []});
+          });
 
-      // iterate tasks to fill all days
-      });
+          // iterate tasks to fill all_days' intervals
+          all_days = _.map(tasks, function(task, i, tasks){
+            var intervals = [],
+            format = "MM/DD/YYYY hh:mm ",
+            brk = task['Breaks Description'];
+            // parse breaks
+            console.log(break_reg.exec(brk));
+            while((m = break_reg.exec(brk)) !== null){
+              var p = m[1], desc = m[2],
+              psplit = p.match(time_stamp_reg)[0].split('–'); // is not '-' hyphen character
+              var start = psplit[0].substring(0,psplit[0].length-1);
+
+              intervals.push({
+                start: moment(
+                  task['Date']+' '+
+                  start,
+                  format+meridiem(start)
+                ),
+                end: moment(
+                  task['Date']+' '+
+                  psplit[1],
+                  format+meridiem(psplit[1])
+                ),
+                desc: desc,
+                isbreak: true,
+                project: task['Project']
+              });
+
+              console.log('break');
+              console.log(intervals);
+            }
+
+            // task
+            intervals.push({
+              start: moment(
+                task['Date']+' '+task['Start time'],
+                format+meridiem(task['Start time'])
+              ),
+              end: moment(
+                task['Date']+' '+task['End time'],
+                format+meridiem(task['End time'])
+              ),
+              desc: task['Description'],
+              isbreak: false,
+              project: task['Project']
+            });
+            // set task color if not present for project TODO
+            return intervals;
+          });
+          console.log(all_days);
+        });
+
+      }
     }
-  }
-})();
+  })();
 
-timesheet.create({cont:"container", csv:"/assets/winter2013.csv"});
+  timesheet.create({cont:"container", csv:"/assets/winter2015.csv"});
